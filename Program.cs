53164e4a4a4410961218ec11be945fd3f732f868
Leandro <leandrogomesdonascimento@gmail.com>
2025-10-25 16:17:24 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using MinimalApi.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login successful");
+    }
+    return Results.Unauthorized();
+});
+
+app.Run();
