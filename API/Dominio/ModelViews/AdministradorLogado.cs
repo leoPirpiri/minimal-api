8f4992fb04541b2e02853161d530298d6fea5e60
Leandro <leandrogomesdonascimento@gmail.com>
2025-10-29 00:02:33 -0300

    Atribuição para classe pública do Model view AdmLogado.

--- API/Dominio/ModelViews/AdministradorLogado.cs
+++ API/Dominio/ModelViews/AdministradorLogado.cs
@@ -1,7 +1,7 @@
 namespace MinimalApi.Dominio.ModelViews
 {
 
-    internal class AdministradorLogado
+    public record AdministradorLogado
     {
         public string Email { get; set; } = default!;
         public string Perfil { get; set; } = default!;
